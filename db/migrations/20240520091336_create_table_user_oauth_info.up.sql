286ec1bd3ccb26dbb4929f9e62d51ba2fd8f2182
Leonard Zonaphan <leonard.zonaphan@gdn-commerce.com>
2024-05-31 00:35:05 +0700

    remove user oauth info entity

--- db/migrations/20240520091336_create_table_user_oauth_info.up.sql
+++
@@ -1,7 +0,0 @@
-create table user_oauth_info (
-  id bigint primary key auto_increment not null,
-  oauth_provider varchar(20) not null,
-  access_token varchar(40) not null,
-  refresh_token varchar(40) not null,
-  expiry_date timestamp not null
-);
