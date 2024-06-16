965d2cae4f67163261f66af86b84e6c4c91fd1dc
Leonard Zonaphan <leonard.zonaphan@gdn-commerce.com>
2024-06-16 23:04:22 +0700

    add unique to user google id

--- db/migrations/20240520090935_create_table_users.up.sql
+++ db/migrations/20240520090935_create_table_users.up.sql
@@ -3,7 +3,7 @@ create table users (
   email varchar(255) not null unique,
   name varchar(255) not null,
   password varchar(255) null,
-  google_id varchar(255) null,
+  google_id varchar(255) null unique,
   created_at bigint not null,
   updated_at bigint not null
 );
