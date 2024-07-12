f084066a10e07bc4c54cda10dba86a9efdb4f7e4
Leonard Zonaphan <leonard.zonaphan@gdn-commerce.com>
2024-07-12 14:35:30 +0700

    upgrade go version, add air config, and use cors default config

--- internal/delivery/http/middleware/cors_middleware.go
+++ internal/delivery/http/middleware/cors_middleware.go
@@ -5,11 +5,6 @@ import (
 	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
-var corsConfig = cors.Config{
-	AllowOrigins: "http://localhost:5173",
-	AllowHeaders: "Origin, Content-Type, Accept",
-}
-
 func NewCors() fiber.Handler {
-	return cors.New(corsConfig)
+	return cors.New(cors.ConfigDefault)
 }
