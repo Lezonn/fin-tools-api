a02702baab95391b2fc05cca6811348ef2d71c36
Leonard Zonaphan <leonard.zonaphan@gdn-commerce.com>
2024-07-31 16:23:00 +0700

    Add create expense API

--- internal/delivery/http/expense_controller.go
+++ internal/delivery/http/expense_controller.go
@@ -1,7 +1,12 @@
 package http
 
 import (
+	"net/http"
+
+	"github.com/Lezonn/fin-tools-api/internal/delivery/http/middleware"
+	"github.com/Lezonn/fin-tools-api/internal/model"
 	"github.com/Lezonn/fin-tools-api/internal/service"
+	"github.com/gofiber/fiber/v3"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 )
@@ -17,4 +22,28 @@ func NewExpenseController(logger *logrus.Logger, service *service.ExpenseService
 		Log:     logger,
 		Service: service,
 	}
+}
+
+func (c *ExpenseController) Create(ctx fiber.Ctx) error {
+	auth := middleware.GetUser(ctx)
+
+	request := &model.CreateExpenseRequest{}
+	if err := ctx.Bind().Body(request); err != nil {
+		c.Log.WithError(err).Error("failed to create expense")
+		return fiber.ErrBadRequest
+	}
+
+	request.UserID = auth.ID
+
+	response, err := c.Service.Create(ctx.UserContext(), request)
+	if err != nil {
+		c.Log.WithError(err).Error("failed to create expense")
+		return err
+	}
+
+	return ctx.JSON(model.WebResponse{
+		Code:   http.StatusOK,
+		Status: http.StatusText(http.StatusOK),
+		Data:   response,
+	})
 }
--- internal/service/expense_service.go
+++ internal/service/expense_service.go
@@ -1,8 +1,14 @@
 package service
 
 import (
+	"context"
+
+	"github.com/Lezonn/fin-tools-api/internal/entity"
+	"github.com/Lezonn/fin-tools-api/internal/model"
+	"github.com/Lezonn/fin-tools-api/internal/model/converter"
 	"github.com/Lezonn/fin-tools-api/internal/repository"
 	"github.com/go-playground/validator/v10"
+	"github.com/gofiber/fiber/v3"
 	"github.com/sirupsen/logrus"
 	"gorm.io/gorm"
 )
@@ -22,4 +28,34 @@ func NewExpenseService(db *gorm.DB, logger *logrus.Logger, validate *validator.V
 		Validate:          validate,
 		ExpenseRepository: expenseRepository,
 	}
+}
+
+func (s *ExpenseService) Create(ctx context.Context, request *model.CreateExpenseRequest) (*model.ExpenseResponse, error) {
+	tx := s.DB.WithContext(ctx).Begin()
+	defer tx.Rollback()
+
+	if err := s.Validate.Struct(request); err != nil {
+		s.Log.WithError(err).Error("failed to validate request body")
+		return nil, fiber.ErrBadRequest
+	}
+
+	expense := &entity.Expense{
+		UserID:            request.UserID,
+		ExpenseCategoryID: request.ExpenseCategoryID,
+		Amount:            request.Amount,
+		Note:              request.Note,
+		ExpenseDate:       request.ExpenseDate,
+	}
+
+	if err := s.ExpenseRepository.Create(tx, expense); err != nil {
+		s.Log.WithError(err).Error("failed to create expense")
+		return nil, fiber.ErrInternalServerError
+	}
+
+	if err := tx.Commit().Error; err != nil {
+		s.Log.WithError(err).Error("failed to commit transaction")
+		return nil, fiber.ErrInternalServerError
+	}
+
+	return converter.ExpenseToResponse(expense), nil
 }
